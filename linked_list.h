6b14eac9c3fac6c1e14183a6953272e3dc414bb0
renan.ikeda <renan.ikeda@ibotz.com.br>
2025-01-09 13:35:16 -0300

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -1,30 +1,34 @@
-// linked_list.h
-
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
-// Node structure
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure definitions
 typedef struct Node {
     int value;
     struct Node *next;
 } Node;
 
-// List structure
 typedef struct List {
     Node *head;
 } List;
 
-// Function prototypes
+// Function declarations
 List *createList(Node *head);
 Node *createNode(int value);
 List *generateList(int values[], int len);
 void addNode(List *list, int value);
+Node *getTail(List *list);
+void pushNode(List *list, int value);
 void deleteList(List *list);
 void deleteNodeByValue(List *list, int value);
 void deleteNode(List *list, Node *node);
 Node *findNode(List *list, int value);
 void swapNodesByValue(List *list, int value1, int value2);
 void swapNodes(List *list, Node *node1, Node *node2);
+List *cloneList(List *list);
+int popList(List *list);
 void printList(List *list);
 void printNode(Node *node);
 
@@ -33,5 +37,7 @@ List *testInstanceList();
 void testAdd();
 void testDeleteNode();
 void testSwap();
+void testClone();
+void testPop();
 
 #endif // LINKED_LIST_H

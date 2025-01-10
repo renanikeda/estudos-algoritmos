8cdb512381046a2ee5c330428ceda0b420cc1b59
renan.ikeda <renan.ikeda@ibotz.com.br>
2025-01-10 15:41:44 -0300

    Adjust headers

--- linked_list.h
+++ linked_list.h
@@ -4,41 +4,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Struct Definitions
+// Definition of Node structure
 typedef struct Node {
     int value;
-    struct Node *next;
+    struct Node* next;
 } Node;
 
+// Definition of List structure
 typedef struct List {
-    Node *head;
+    Node* head;
 } List;
 
-// Function Prototypes
-List *createList(Node *head);
-Node *createNode(int value);
-List *generateList(int values[], int len);
-void addNode(List *list, int value);
-Node *getTail(List *list);
-void pushNode(List *list, int value);
-void deleteList(List *list);
-void deleteNodeByValue(List *list, int value);
-void deleteNode(List *list, Node *node);
-Node *findNode(List *list, int value);
-void swapNodesByValue(List *list, int value1, int value2);
-void swapNodes(List *list, Node *node1, Node *node2);
-List *cloneList(List *list);
-int popList(List *list);
-List *invertList(List *list);
-void invertListInplace(List *list);
-void insertSorted(List *list, int value);
-List *sortList(List *list);
-void sortListInplace(List *list);
-void printList(List *list);
-void printNode(Node *node);
+// Function prototypes
+List* createList(Node* head);
+Node* createNode(int value);
+List* generateList(int values[], int len);
+void addNode(List* list, int value);
+Node* getTail(List* list);
+void pushNode(List* list, int value);
+void deleteList(List* list);
+void deleteNodeByValue(List* list, int value);
+void deleteNode(List* list, Node* node);
+Node* findNode(List* list, int value);
+void swapNodesByValue(List* list, int value1, int value2);
+void swapNodes(List* list, Node* node1, Node* node2);
+List* cloneList(List* list);
+int popList(List* list);
+List* invertList(List* list);
+void invertListInplace(List* list);
+void insertSorted(List* list, int value);
+List* sortList(List* list);
+void sortListInplace(List* list);
+int* getBucket(int maxValue, List* list);
+void removeDuplicatesBucket(List* list);
+void printList(List* list);
+void printNode(Node* node);
 
-// Test Function Prototypes
-List *testInstanceList();
+// Test functions
+List* testInstanceList();
 void testAdd();
 void testDeleteNode();
 void testSwap();
@@ -46,5 +49,6 @@ void testClone();
 void testPop();
 void testInvert();
 void testInsert();
+void testDuplicates();
 
 #endif // LINKED_LIST_H

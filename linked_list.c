5cae82ad191e845d65d65cc32f7a8df5039efa9b
renan.ikeda <renan.ikeda@ibotz.com.br>
2025-01-10 15:34:03 -0300

    Fix: pointer syntax

--- linked_list.c
+++ linked_list.c
@@ -3,33 +3,33 @@
 #include "linked_list.h"
 
 
-List *createList(Node *head) {
-	List *list = (List *)malloc(sizeof(List));
+List* createList(Node* head) {
+	List* list = (List *)malloc(sizeof(List));
 	list->head = head;
 	return list;
 }
 
-Node *createNode(int value) {
-	Node *node = (Node *) malloc(sizeof(Node));
+Node* createNode(int value) {
+	Node* node = (Node *) malloc(sizeof(Node));
 	node->value = value;
 	node->next = NULL;
 	return node;
 }
 
 List* generateList(int values[], int len) {
-	List *list = createList(NULL);
+	List* list = createList(NULL);
     for(int i = len -1; i >= 0; i--) addNode(list, values[i]);
 }
 
-void addNode(List *list, int value)  {
-	Node *node = createNode(value);
+void addNode(List* list, int value)  {
+	Node* node = createNode(value);
 	node->next = list->head;
 	list->head = node;
 }
 
-Node* getTail(List *list) {
-    Node *prev = list->head;
-    Node *curr = list->head;
+Node* getTail(List* list) {
+    Node* prev = list->head;
+    Node* curr = list->head;
     while(curr != NULL) {
         prev = curr;
         curr = curr->next;
@@ -37,9 +37,9 @@ Node* getTail(List *list) {
     return prev;
 }
 
-void pushNode(List *list, int value) {  
-    Node *node = createNode(value);
-    Node *tail = getTail(list);
+void pushNode(List* list, int value) {  
+    Node* node = createNode(value);
+    Node* tail = getTail(list);
     if (tail == NULL) {
         list->head = node;
     } else {
@@ -47,10 +47,10 @@ void pushNode(List *list, int value) {
     }
 }
 
-void deleteList(List *list) {
-	Node *current = list->head;
+void deleteList(List* list) {
+	Node* current = list->head;
 	while (current != NULL) {
-		Node *temp = current;
+		Node* temp = current;
 		printf("Deleting Node %d\n", temp->value);
         current = current->next;
 		free(temp);
@@ -58,14 +58,14 @@ void deleteList(List *list) {
 	list->head = NULL;
 }
 
-void deleteNodeByValue(List *list, int value) {
+void deleteNodeByValue(List* list, int value) {
     if (list->head == NULL) {
         printf("The list is empty. Cannot delete.\n");
         return;
     }
 
-    Node *current = list->head;
-    Node *previous = NULL;
+    Node* current = list->head;
+    Node* previous = NULL;
 
     while (current != NULL) {
         if (current->value == value) {
@@ -85,14 +85,14 @@ void deleteNodeByValue(List *list, int value) {
     printf("Node with value %d not found.\n", value);
 }
 
-void deleteNode(List *list, Node *node) {
+void deleteNode(List* list, Node* node) {
     if (list->head == NULL) {
         printf("The list is empty. Cannot delete.\n");
         return;
     }
 
-    Node *current = list->head;
-    Node *previous = NULL;
+    Node* current = list->head;
+    Node* previous = NULL;
 
     while (current != NULL) {
         if (current == node) {
@@ -112,8 +112,8 @@ void deleteNode(List *list, Node *node) {
     printf("Node with value %d not found.\n", node->value);
 }
 
-Node* findNode(List *list, int value) {
-	Node *current = list->head;
+Node* findNode(List* list, int value) {
+	Node* current = list->head;
 	while (current != NULL) {
         if (current->value == value) return current;
         current = current->next;
@@ -121,25 +121,25 @@ Node* findNode(List *list, int value) {
 }
 
 
-void swapNodesByValue(List *list, int value1, int value2) {
+void swapNodesByValue(List* list, int value1, int value2) {
     if (list->head == NULL) {
         printf("The list is empty. Cannot swap.\n");
         return;
     }
-    Node *node1 = findNode(list, value1);
-    Node *node2 = findNode(list, value2);
+    Node* node1 = findNode(list, value1);
+    Node* node2 = findNode(list, value2);
 
     swapNodes(list, node1, node2);
 
 }
 
-void swapNodes(List *list, Node *node1, Node *node2) {
+void swapNodes(List* list, Node* node1, Node* node2) {
     if (node1 == NULL || node2 == NULL) {
         printf("One or both nodes not found; cannot swap.\n");
         return;
     }
-    Node *current = list->head;
-    Node *previous = NULL;
+    Node* current = list->head;
+    Node* previous = NULL;
     
     while (current != NULL) {
         printNode(current);
@@ -160,15 +160,15 @@ void swapNodes(List *list, Node *node1, Node *node2) {
         current = current->next;
     }
 
-    Node *temp = node1->next;
+    Node* temp = node1->next;
     node1->next = node2->next;
     node2->next = temp;
 
 }
 
-List* cloneList(List *list) {
-    List *new_list = createList(NULL);
-    Node *head = list->head;
+List* cloneList(List* list) {
+    List* new_list = createList(NULL);
+    Node* head = list->head;
     while(head != NULL) {
         pushNode(new_list, head->value);
         head = head->next;
@@ -176,7 +176,7 @@ List* cloneList(List *list) {
     return new_list;
 }
 
-int popList(List *list) {
+int popList(List* list) {
     Node* tail = getTail(list);
     int value = tail->value;
     deleteNode(list, tail);
@@ -184,23 +184,23 @@ int popList(List *list) {
 }
 
 
-List* invertList(List *list) {
-    List *new_list = createList(NULL);
-    Node *head = list->head;
+List* invertList(List* list) {
+    List* new_list = createList(NULL);
+    Node* head = list->head;
     while (head != NULL) {
         addNode(new_list, head->value);
-        Node *temp = head;
+        Node* temp = head;
         head = head->next;
         deleteNode(list, temp);
     }
     return new_list;
 }
 
-void invertListInplace(List *list) {
-    Node *prev = NULL;
-    Node *head = list->head;
+void invertListInplace(List* list) {
+    Node* prev = NULL;
+    Node* head = list->head;
     while (head != NULL) {
-        Node *next_head = head->next;
+        Node* next_head = head->next;
         if (prev == NULL) {
             head->next = NULL;
         } else {
@@ -213,8 +213,8 @@ void invertListInplace(List *list) {
     return;
 }
 
-void insertSorted(List *list, int value) {
-    Node *curr = list->head;
+void insertSorted(List* list, int value) {
+    Node* curr = list->head;
     while (curr != NULL && curr->value < value) {
         curr = curr->next;
     }
@@ -223,16 +223,16 @@ void insertSorted(List *list, int value) {
         pushNode(list, value);
         return;
     } else {
-        Node *new_node = createNode(value);
-        Node *next_node = curr->next;
+        Node* new_node = createNode(value);
+        Node* next_node = curr->next;
         curr->next = new_node;
         new_node->next = next_node;
     }
 }
 
-List* sortList(List *list) {
-    List *new_list = createList(NULL);
-    Node *curr = list->head;
+List* sortList(List* list) {
+    List* new_list = createList(NULL);
+    Node* curr = list->head;
     if (curr == NULL) {
         return new_list;
     }
@@ -249,8 +249,8 @@ void sortListInplace(List* list) {
 }
 
 int* getBucket(int maxValue, List* list) {
-    int *bucket = (int*) calloc(maxValue, sizeof(int));
-    Node *curr = list->head;
+    int* bucket = (int*) calloc(maxValue, sizeof(int));
+    Node* curr = list->head;
     while (curr != NULL){
         bucket[curr->value] += 1;
         curr = curr->next;
@@ -260,9 +260,9 @@ int* getBucket(int maxValue, List* list) {
 
 void removeDuplicatesBucket(List* list){
     int maxValue = 100;
-    int *bucket = getBucket(maxValue, list);
+    int* bucket = getBucket(maxValue, list);
 
-    Node *curr = list->head;
+    Node* curr = list->head;
     while (curr != NULL){
         Node* next_node = curr->next;
         if (bucket[curr->value] > 1) {
@@ -273,12 +273,12 @@ void removeDuplicatesBucket(List* list){
     }
 }
 
-void printList(List *list) {
+void printList(List* list) {
 	if (list->head == NULL) {
         printf("Empty list\n");
         return;
     }
-	Node *current = list->head;
+	Node* current = list->head;
 	printf("HEAD -> ");
     while (current != NULL) {
         printf("%d -> ", current->value);
@@ -288,7 +288,7 @@ void printList(List *list) {
     return;
 }
 
-void printNode(Node *node) {
+void printNode(Node* node) {
 	if (node == NULL) {
         printf("Node: NULL\n");   
         return;
@@ -299,19 +299,19 @@ void printNode(Node *node) {
 List* testInstanceList() {
     int values[] = {0, 1, 2, 3, 4, 5};
     int len = sizeof(values) / sizeof(values[0]);
-	List *list = generateList(values, len);
+	List* list = generateList(values, len);
 	return list;
 }
 
 void testAdd() {
-	List *list = testInstanceList();
+	List* list = testInstanceList();
 	printList(list);
 };
 
 void testDeleteNode() {
-	List *list = testInstanceList();
+	List* list = testInstanceList();
 	printList(list);
-	Node *nodeToDelete = list->head->next;
+	Node* nodeToDelete = list->head->next;
 	printNode(nodeToDelete);
 	deleteNode(list, nodeToDelete);
 	printList(list);
@@ -320,29 +320,29 @@ void testDeleteNode() {
 };
 
 void testSwap() {
-    List *list = testInstanceList();
+    List* list = testInstanceList();
     printList(list);
 	swapNodesByValue(list, 30, 20);
     printList(list);
 }
 
 void testClone() {
-    List *list = testInstanceList();
+    List* list = testInstanceList();
     printList(list);
-    List *new_list = cloneList(list);
+    List* new_list = cloneList(list);
     printList(new_list);
 }
 void testPop() {
-    List *list = testInstanceList();
+    List* list = testInstanceList();
     printList(list);
     int popped_value = popList(list);
     printList(list);
 }
 
 void testInvert() {
-    List *list = testInstanceList();
+    List* list = testInstanceList();
     printList(list);
-    List *inverted_list = invertList(list);
+    List* inverted_list = invertList(list);
     printList(inverted_list);
     printList(list);
     invertListInplace(inverted_list);
@@ -350,14 +350,14 @@ void testInvert() {
 }
 
 void testInsert() {
-    List *list = testInstanceList();
+    List* list = testInstanceList();
     printList(list);
-    List *new_list = sortList(list);
+    List* new_list = sortList(list);
     printList(new_list);
 }
 
 void testDuplicates() {
-    List *list = testInstanceList();
+    List* list = testInstanceList();
     insertSorted(list, 3);
     insertSorted(list, 5);
     printList(list);
